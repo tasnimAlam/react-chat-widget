0dee6796e641f1d340c601dcbaa793950e5243ad
Tasnim Alam <tasnimalamcse@gmail.com>
2019-09-19 12:15:28 +0600

    Clean code

--- src/store/dispatcher.js
+++ src/store/dispatcher.js
@@ -1,14 +1,6 @@
 import store from "./store";
 import * as actions from "./actions";
 
-export function toggleMsgLoader() {
-  store.dispatch(actions.toggleMsgLoader());
-}
-
-export function renderCustomComponent(component, props, showAvatar = false) {
-  store.dispatch(actions.renderCustomComponent(component, props, showAvatar));
-}
-
 export function toggleWidget() {
   store.dispatch(actions.toggleChat());
 }
